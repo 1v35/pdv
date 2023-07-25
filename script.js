009a9c0af3e8f49432d2c9631907b7828904cf0d
1v35 <ivaldo.raul@hotmail.com>
2023-07-25 08:03:30 -0300

    db Firebase

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-// Simulated client data
-const clients = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Smith" },
-  { id: 3, name: "Alex Johnson" },
-];
+// Seu web app Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI3rtJI3lJ6B3VC7Mren0X3PypZnUBOrs",
+  authDomain: "pedeve-bd523.firebaseapp.com",
+  projectId: "pedeve-bd523",
+  storageBucket: "pedeve-bd523.appspot.com",
+  messagingSenderId: "610962852482",
+  appId: "1:610962852482:web:c4ee23b53228457d01029b",
+  measurementId: "G-WXLS7HSH2L"
+};
 
-// Function to search for a client by ID
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Obtenha uma referência para o banco de dados do Firebase
+const database = firebase.database();
+
+// Função para pesquisar um cliente pelo ID
 function searchClient() {
   const clientId = document.getElementById("clientIdInput").value;
   const resultContainer = document.getElementById("resultContainer");
 
-  const client = clients.find((c) => c.id === parseInt(clientId));
-
-  if (client) {
-    resultContainer.innerText = "Client found: " + client.name;
-  } else {
-    resultContainer.innerText = "Client not found";
-  }
+  // Use o banco de dados do Firebase para obter os dados do cliente
+  database.ref("clients/" + clientId).once("value")
+    .then((snapshot) => {
+      const client = snapshot.val();
+      if (client) {
+        resultContainer.innerText = "Cliente encontrado: " + client.name;
+      } else {
+        resultContainer.innerText = "Cliente não encontrado";
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar dados:", error);
+    });
 }
 
-// Attach click event listener to the search button
+// Adicione um evento de clique ao botão de pesquisa
 document.getElementById("searchButton").addEventListener("click", searchClient);
